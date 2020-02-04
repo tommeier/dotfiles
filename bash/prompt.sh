23772e0d651f80b77c244f7115ba404aedc1cd1a
Tom Meier <tom@venombytes.com>
2020-02-05 09:53:43 +1100

    Disable kube prompt for now

--- bash/prompt.sh
+++ bash/prompt.sh
@@ -6,21 +6,22 @@ if [ -f "$(brew --prefix)/opt/bash-git-prompt/share/gitprompt.sh" ]; then
   # GIT_PROMPT_THEME=Single_line_DarkTom_Custom # custom settings in ~/.git-prompt-colors.sh
   GIT_PROMPT_ONLY_IN_REPO=0
 
-  # brew install kube-ps1
-  if [ -f "$(brew --prefix)/opt/kube-ps1/share/kube-ps1.sh" ]; then
-    source "$(brew --prefix)/opt/kube-ps1/share/kube-ps1.sh"
-    KUBE_PS1_NS_ENABLE="false"
-    # KUBE_PS1_SYMBOL_USE_IMG="true"
-    KUBE_PS1_SYMBOL_COLOR="white"
-    function prompt_callback {
-      local kube_result=$(kube_ps1)
-      if [[ "$kube_result" != "" ]]; then
-        echo '[$(kube_ps1)]'
-      fi
-    }
-  else
-    echo "[dotfiles:prompt.sh] 'brew install kube-ps1' for kube prompt"
-  fi
+  # Disable - not doing as much kube now
+  # # brew install kube-ps1
+  # if [ -f "$(brew --prefix)/opt/kube-ps1/share/kube-ps1.sh" ]; then
+  #   source "$(brew --prefix)/opt/kube-ps1/share/kube-ps1.sh"
+  #   KUBE_PS1_NS_ENABLE="false"
+  #   # KUBE_PS1_SYMBOL_USE_IMG="true"
+  #   KUBE_PS1_SYMBOL_COLOR="white"
+  #   function prompt_callback {
+  #     local kube_result=$(kube_ps1)
+  #     if [[ "$kube_result" != "" ]]; then
+  #       echo '[$(kube_ps1)]'
+  #     fi
+  #   }
+  # else
+  #   echo "[dotfiles:prompt.sh] 'brew install kube-ps1' for kube prompt"
+  # fi
   source "$(brew --prefix)/opt/bash-git-prompt/share/gitprompt.sh"
 else
   echo "[dotfiles:prompt.sh] 'brew install bash-git-prompt' for git prompt"
