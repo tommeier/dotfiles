81d024316d4d316fd44eacabdf4a0e6a0b56a7ec
Tom Meier <tom@venombytes.com>
2020-11-18 15:23:15 +1100

    Things

--- bash/exports.sh
+++ bash/exports.sh
@@ -4,3 +4,6 @@ export EDITOR='subl --wait'
 # Prefer AU English and use UTF-8
 export LANG="en_AU"
 export LC_ALL="en_AU.UTF-8"
+
+# Disable docker for local dev and use asdf
+export DEV_DOCKER="false"
