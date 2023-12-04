6d3c286b3ae9d5f595d18e66c53e1176e9760429
Tom Meier <tom@venombytes.com>
2023-12-05 09:03:40 +1100

    Update zsh automatically

--- terminal/zsh/theme.sh
+++ terminal/zsh/theme.sh
@@ -42,7 +42,7 @@ POWERLEVEL9K_MULTILINE_LAST_PROMPT_PREFIX="%{%B%F{black}%K{yellow}%} $user_symbo
 
 # Uncomment one of the following lines to change the auto-update behavior
 # zstyle ':omz:update' mode disabled  # disable automatic updates
-# zstyle ':omz:update' mode auto      # update automatically without asking
+zstyle ':omz:update' mode auto      # update automatically without asking
 # zstyle ':omz:update' mode reminder  # just remind me to update when it's time
 
 # Uncomment the following line to change how often to auto-update (in days).
